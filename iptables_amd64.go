8c767912118a2608ecb83a81c60c324dd9433a2e
Michael Stapelberg <michael@stapelberg.de>
2022-04-14 13:03:43 +0200

    convert package to provide architecture-dependent extrafiles tar
    related to https://github.com/gokrazy/podman/issues/2

---
+++ iptables_amd64.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	const frozenDir = "/usr/lib/x86_64-linux-gnu/iptables.frozen"
+	iptables := exec.Command(
+		frozenDir+"/ld-linux-x86-64.so.2",
+		append([]string{
+			frozenDir + "/iptables",
+		}, os.Args[1:]...)...)
+	iptables.Env = append(os.Environ(), "LD_LIBRARY_PATH="+frozenDir)
+	iptables.Stdin = os.Stdin
+	iptables.Stdout = os.Stdout
+	iptables.Stderr = os.Stderr
+	err := iptables.Run()
+	if err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				os.Exit(status.ExitStatus())
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "%v: %v\n", iptables.Args, err)
+		}
+	}
+}
